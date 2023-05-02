060c86c38f48e5c4b260dcc48befd9f1f05ce766
Amit Sarker <amitsarkerstock@gmail.com>
2023-05-03 01:54:40 +0600

    Chef section done

--- src/Layout/components/Home/Home.jsx
+++ src/Layout/components/Home/Home.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
+import { FaUserGraduate, FaList, FaSmile } from "react-icons/fa";
 
 const Home = () => {
   const chef = useLoaderData();
-  console.log(chef);
+  //   console.log(chef);
   return (
     <div>
       {/* Hero area/Banner */}
       <div className="bg-[url('https://img.freepik.com/free-photo/vegetables-set-left-black-slate_1220-685.jpg?w=1480&t=st=1683051760~exp=1683052360~hmac=cab5ee4c18593bfc4dfcdb8b75a2f02b786fe424a3d5dc6cd9380d8848f4f929')] bg-cover py-48">
         <div className="container px-4 md:px-0 mx-auto">
           <h1 className="text-5xl text-white font-bold">
-            Explore the Flavors of Mexico
+            Explore the Flavors of{" "}
+            <span className="text-orange-500">Mexico</span>
           </h1>
           <p className="text-white  text-lg mt-7">
             Discover the Vibrant World of Mexican Cuisine with our Collection of
@@ -22,6 +24,42 @@ const Home = () => {
             Explore Now
           </button>
         </div>
+      </div>
+
+      {/* Chef area */}
+      <div className="container mx-auto grid grid-cols-1 gap-10 md:grid-cols-3 text-center my-24">
+        {chef.map((singleChef) => (
+          <div className="text-center mb-20">
+            <img
+              className=" mx-auto rounded-full h-96 w-96 object-cover"
+              src={singleChef.chef_picture_url}
+              alt=""
+            />
+            <h1 className="text-3xl font-bold mt-4 text-gray-700">
+              {singleChef.chef_name}
+            </h1>
+            <div className="flex justify-evenly gap-2 mt-4">
+              <div className="flex items-center gap-2 font-medium py-2 px-3 bg-blue-100 rounded-xl text-gray-600">
+                <FaUserGraduate />
+                <span>Ex : {singleChef.experience} </span>
+              </div>
+              <div className="flex items-center gap-2 font-medium py-2 px-3 bg-blue-100 rounded-xl text-gray-600">
+                <FaList />
+                <span>Total Recipes : {singleChef.recipes.length} </span>
+              </div>
+              <div className="flex items-center gap-2 font-medium py-2 px-3 bg-blue-100 rounded-xl text-gray-600">
+                <FaSmile />
+                <span>Likes : {singleChef.number_of_likes} </span>
+              </div>
+            </div>
+            <h3 className="font-xl font-medium mt-4 text-gray-600">
+              {singleChef.short_bio}
+            </h3>
+            <button className="py-3 px-8 rounded-lg bg-orange-500 font-medium text-white mt-4">
+              <Link>View All Recipes</Link>
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,8 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
-        loader: () => fetch("https://mexican-food-server.vercel.app/chef"),
+        loader: () =>
+          fetch("https://mexican-food-server-amitsarkerui.vercel.app/chef"),
       },
     ],
   },
